c574e562cae6cb7c83d0e12029e969cecfa7737f
Fl3mish <mitch.pauwels@proton.me>
2024-08-09 21:59:13 +0100

    Changed logic into more readable code / decreased  btn container size

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,24 +3,22 @@ import React, { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  //   function handleClick(action) {
-  //     if (action === "increment") setCount((prev) => prev + 1);
-  //     if (action === "decrement") setCount((prev) => prev - 1);
-  //   }
+  function handleIncrement() {
+    setCount(count + 1);
+  }
 
-  function handleClick(e) {
-    if (e.target.textContent === "+") setCount(count + 1);
-    if (e.target.textContent === "-") setCount(count - 1);
+  function handleDecrement() {
+    setCount(count - 1);
   }
 
   return (
     <div className="container">
       <h1 className="number">{count}</h1>
       <section className="btns-container">
-        <button onClick={handleClick} className="btn">
+        <button onClick={handleDecrement} className="btn">
           -
         </button>
-        <button onClick={handleClick} className="btn">
+        <button onClick={handleIncrement} className="btn">
           +
         </button>
       </section>
