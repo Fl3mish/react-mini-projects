f70765e2ba98be481b77b054a701719777cccde3
Fl3mish <mitch.pauwels@proton.me>
2024-08-10 11:05:25 +0100

    Calculator component/Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import Counter from "./components/Counter";
 // import TodoList from "./components/TodoList";
-import MealApi from "./components/MealApi";
-import "./styles.css";
+// import MealApi from "./components/MealApi";
+import Calculator from "./components/calculator/Calculator";
+// import "./styles.css";
 function App() {
   return (
     <div>
       {/* <Counter /> */}
       {/* <TodoList /> */}
-      <MealApi />
+      {/* <MealApi /> */}
+      <Calculator />
     </div>
   );
 }
