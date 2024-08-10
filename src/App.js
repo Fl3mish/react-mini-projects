155bac33cf02226d9d6b3915f89974923db9bc7e
Fl3mish <mitch.pauwels@proton.me>
2024-08-10 10:22:39 +0100

    Mealapi Component/Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Counter from "./components/Counter";
-import TodoList from "./components/TodoList";
+// import TodoList from "./components/TodoList";
+import MealApi from "./components/MealApi";
 import "./styles.css";
 function App() {
   return (
     <div>
       {/* <Counter /> */}
-      <TodoList />
+      {/* <TodoList /> */}
+      <MealApi />
     </div>
   );
 }
---
+++ src/components/MealApi.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+export default function MealApi() {
+  const [items, setItems] = useState([]);
+
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood");
+  //         const data = await response.json();
+  //         setItems(data);
+  //       } catch (error) {
+  //         console.log("Error fetching data:", error);
+  //       }
+  //     };
+  //     fetchData();
+  //   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+        ); //imediately does json parsing
+        console.log(response.data.meals);
+        setItems(response.data.meals);
+      } catch (error) {
+        if (error.response) {
+          console.error(error.response.data); // Server responded with an error
+          console.error(error.response.status); // Status code of the error
+        } else if (error.request) {
+          console.error("Request made but no response received");
+        } else {
+          console.error("Error setting up the request:", error.message);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="items-container">
+      {items.map((item) => (
+        <section className="card" key={item.idMeal}>
+          <img src={item.strMealThumb} alt={item.strMeal} />
+          <section className="content">
+            <p>{item.strMeal}</p>
+            <p>#{item.idMeal}</p>
+          </section>
+        </section>
+      ))}
+    </div>
+  );
+}
